2eae3a4af7df9ede843e1efd7afa5eef9914f4f2
IsaC-8bBit (laptop) <isaiascedillo1313@gmail.com>
2025-04-08 11:09:55 -0600

    commit

--- PrototipoP12P2025/PrototipoP12P2025/src/NotasParciales.cpp
+++ PrototipoP12P2025/PrototipoP12P2025/src/NotasParciales.cpp
@@ -7,10 +7,172 @@
 #include <iomanip>
 
 using namespace std;
-NotasParciales::NotasParciales(float matriz_facultad_1, float matriz_facultad_2, float matriz_facultad_3)
+NotasParciales::NotasParciales()
 {
-    this->matriz_facultad_1=matriz_facultad_1;
-    this->matriz_facultad_2=matriz_facultad_2;
-    this->matriz_facultad_3=matriz_facultad_3;
+}
+void NotasParciales::llamadaCiclo()
+{
+    //Declarar Matrices por facultad
+    float matriz_facultad_1[NUMERO_ALUMNOS][NUMERO_NOTAS + 1];
+    float matriz_facultad_2[NUMERO_ALUMNOS][NUMERO_NOTAS + 1];
+    float matriz_facultad_3[NUMERO_ALUMNOS][NUMERO_NOTAS + 1];
+    //Bool de While
+    char opcion;
+
+    bool repetir = true;
+    //Variables de calculo de promedio de Facultades
+    float promedio_facultad_1 = 0;
+    float promedio_facultad_2 =0;
+    float promedio_facultad_3 =0;
+
+    //Nombres de alumnos
+    char alumnos[NUMERO_ALUMNOS][MAXIMA_LONGITUD_CADENA] = {"Carlos","Luis","Maria","Pedro","Juan"};
+    //While de facultadoes
+    do
+    {
+        //Limpiar la consola
+        system("cls");
+
+        //Inicializar los valores
+        promedio_facultad_1=0;
+        promedio_facultad_2=0;
+        promedio_facultad_3=0;
+        //Crear cuadro de notas de facultades
+        cout << "*** Comparativo de Facultades ***" << endl << endl;
+        llenarMatriz(matriz_facultad_1); //Calculo de las notas por facultad
+        promedio_facultad_1 = imprimirMatriz(matriz_facultad_1, alumnos, "Facultad de Ingenieria"); //Despliegue de valores por facultad
+        llenarMatriz(matriz_facultad_2);
+        promedio_facultad_2 = imprimirMatriz(matriz_facultad_2, alumnos, "Facultad de Arquitectura");
+        llenarMatriz(matriz_facultad_3);
+        promedio_facultad_3 = imprimirMatriz(matriz_facultad_3, alumnos, "Facultad de Administracion");
+        //Calculo del mejor promedio de las facultades
+        if (promedio_facultad_1 > promedio_facultad_2 && promedio_facultad_1 > promedio_facultad_3)
+        {
+            //Facultad 1 es mayor
+            cout << " La facultad con el mejor promedio es : " << "Facultad de Ingenieria" << " Promedio: " << promedio_facultad_1 << endl;
+        } else
+        if (promedio_facultad_2 > promedio_facultad_1 && promedio_facultad_2 > promedio_facultad_3)
+        {
+            //Facultad 2 es mayor
+            cout << " La facultad con el mejor promedio es : " << "Facultad de Arquitectura" << " Promedio: " << promedio_facultad_2 << endl;
+        } else
+        if (promedio_facultad_3 > promedio_facultad_2 && promedio_facultad_3 > promedio_facultad_1)
+        {
+            //Facultad 3 es mayor
+            cout << " La facultad con el mejor promedio es : " << "Facultad de Administracion" << " Promedio: " << promedio_facultad_3 << endl;
+        } else
+        {
+            //Varias facultades son iguales en promedio
+            cout << " Algunas facultades tienen el mismo promedio " << endl << endl;
+        }
+        cout << "Desea otro calculo (s/n)? ";
+        cin >> opcion;
+        if (opcion == 'n'|| opcion == 'N' )
+        {
+            repetir=false;
+        }
+    } while (repetir);
+}
+  int NotasParciales::busquedaAleatoreos(int min, int max)
+{
+    //Calculo de valores aleatorios tomando como base la nota minima y màxima del parcial
+    return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+}
+
+  void NotasParciales::llenarMatriz(float matriz[NUMERO_ALUMNOS][NUMERO_NOTAS+1])
+{
+    //Funciòn que permite llenar cada matriz con valores aleatorios
+    int y, x;
+    for (y = 0; y < NUMERO_ALUMNOS; y++)
+    {
+        float suma = 0;
+        int calificacion = 0;
+        for (x = 0; x < NUMERO_NOTAS; x++)
+        {
+            if (x == 0 || x == 3)  //Calculo del primer parcial o actividades
+            {
+                calificacion = busquedaAleatoreos(MIN_CALIFICACION, 20);
+            } else if (x == 1)  //Calculo del segundo parcial
+            {
+                calificacion = busquedaAleatoreos(MIN_CALIFICACION, 25);
+            } else if (x == 2)  //Calculo del examen final
+            {
+                calificacion = busquedaAleatoreos(MIN_CALIFICACION, 35);
+            }
+            suma += (float)calificacion;
+            matriz[y][x] = calificacion;
+            calificacion=0;
+        }
+        // Agregar promedio al final de la matriz
+
+            matriz[y][NUMERO_NOTAS] = suma;
+    }
+}
+
+  void NotasParciales::imprimirMatrizLinea()
+{
+    //Funciòn que despliega titulo de cada nota calculada
+    int x;
+
+    cout << "+--------";
+    for (x = 0; x < NUMERO_NOTAS + 1; x++)
+    {
+        cout << "+---------";
+    }
+    cout << "+\n";
+}
+  float NotasParciales::imprimirMatriz(float matriz[NUMERO_ALUMNOS][NUMERO_NOTAS + 1], char alumnos[NUMERO_ALUMNOS][MAXIMA_LONGITUD_CADENA], string nombreFacultad)
+{
+    //Funciòn que imprime la matriz en pantalla y realizando los calculos necesarios del promedio
+    int y, x;
+
+    float promedioMayor = matriz[0][NUMERO_NOTAS];
+    float promedioMenor = matriz[0][NUMERO_NOTAS];
+    float totalGeneral = 0;
+    float promedioGeneral = 0;
+    char alumnoPromedioMayor[MAXIMA_LONGITUD_CADENA];
+    char alumnoPromedioMenor[MAXIMA_LONGITUD_CADENA];
+    memcpy(alumnoPromedioMayor, alumnos[0], MAXIMA_LONGITUD_CADENA);
+    memcpy(alumnoPromedioMenor, alumnos[0], MAXIMA_LONGITUD_CADENA);
+    cout << nombreFacultad << endl;
+    cout << "(Nota1)=>Primer Parcial  (Nota2)=>Segundo Parcial (Nota3)=>Final (Nota4)=>Actividades" << endl;
+    imprimirMatrizLinea();
+    cout << setw(9) << "Alumno";
+    for (x = 0; x < NUMERO_NOTAS; x++)
+    {
+        cout << setw(9) << "Nota" << x + 1;
+    }
+    cout << setw(8) << "Tot" << endl;
+    imprimirMatrizLinea();
+    for (y = 0; y < NUMERO_ALUMNOS; y++)
+    {
+        cout << "!" << setw(8) << alumnos[y] << "!";
+        float suma = 0;
+        for (x = 0; x < NUMERO_NOTAS; x++)
+        {
+            int calificacion = matriz[y][x];
+            cout << setw(9) << calificacion << "!";
+        }
+        float promedio = matriz[y][NUMERO_NOTAS];
+        totalGeneral += matriz[y][NUMERO_NOTAS];
+        if (promedio > promedioMayor) //Se va guardando la nota mayor y el nombre del alumno
+        {
+            promedioMayor = promedio;
+            memcpy(alumnoPromedioMayor, alumnos[y], MAXIMA_LONGITUD_CADENA);
+        }
+        if (promedio < promedioMenor) //Se va guardando la nota menor y el nombre del alumno
+        {
+            promedioMenor = promedio;
+            memcpy(alumnoPromedioMenor, alumnos[y], MAXIMA_LONGITUD_CADENA);
+        }
+        cout << setw(9) << fixed << setprecision(2) << promedio << "!" << endl;
+        imprimirMatrizLinea();
+    }
+    //Calculo del promedio general de cada facultad
+    promedioGeneral = totalGeneral / NUMERO_ALUMNOS;
+    cout << "Nota mayor: " << setw(10) << alumnoPromedioMayor <<  setw(10) << promedioMayor << endl;
+    cout << "Nota menor: " << setw(10) << alumnoPromedioMenor <<  setw(10) << promedioMenor << endl;
+    cout << "Nota prom : " << setw(10) <<  promedioGeneral << endl << endl;
+    return promedioGeneral; //Retorno del promedio general por facultad
 }
 
